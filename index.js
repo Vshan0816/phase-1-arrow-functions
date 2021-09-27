cff4db946a05165e6ce8746df047c7f003c031a6
Vincent <vshan0816@gmail.com>
2021-09-27 14:07:16 -0400

    commit tree dc477efae631a4c42f01306b2b501fafdb431745

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+    const num1 = 2000
+    const num2 = 100
+    return num1 / num2
+}
+
+const square = number => number*number
+
+const add = (num1, num2) => num1 + num2
